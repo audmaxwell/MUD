1e928f86afdd89c56143d2341da59b9363a2ef8c
Wiithan <55458706+Wiithan@users.noreply.github.com>
2019-09-24 14:59:49 -0400

    Update Items.java

--- minecraft2/src/Items.java
+++ minecraft2/src/Items.java
@@ -2,3 +2,31 @@
 public class Items {
 
 }
+public class Item {
+	private String name;
+	private String desc;
+	private ArrayList<Room> use_room;
+	
+	public Item(String name, String description, ArrayList<Room> use_room) {
+		this.name = name;
+		desc = description;
+		this.use_room = use_room;
+		
+	}
+	
+	
+			
+	
+	public String examine() {
+		return desc;
+	}
+	
+	public void use() {
+		if(use_room.contains(Player.room)) {
+			
+		}else {
+			System.out.println("This item cannot be used in this room.");
+		}
+		
+	}
+}
