f1892dbbf496a8d4366490ff191813acb98b4282
Ethan <55458706+Wiithan@users.noreply.github.com>
2019-09-26 14:26:59 -0400

    Update Room.java

--- minecraft2/src/Room.java
+++ minecraft2/src/Room.java
@@ -1,4 +1,36 @@
 
-public class Room {
+import java.util.ArrayList;
 
+public class Room {
+	private String name;
+	private String desc;
+	private ArrayList<String> exits;
+	private ArrayList<Item> items;
+	public Room(String name, String description, ArrayList<String> exits, ArrayList<Item> items ) {
+		this.name = name;
+		desc = description;
+		
+		
+		
+		
+	}
+	
+	public String look() {
+		return desc;
+	}
+	
+	public void item_add() {
+		
+	}
+	
+	public void exit_add() {
+		
+	}
+	
+	
+	private  ArrayList<Item> item = new ArrayList<Item>();
+	private ArrayList<String> exit = new ArrayList<String>();
+	
+	
+	
 }
