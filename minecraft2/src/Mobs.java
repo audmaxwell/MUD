776ff8b01ab5cd9568268157f06ab558891aec2b
Ethan <55458706+Wiithan@users.noreply.github.com>
2019-10-06 19:39:59 -0400

    Update Mobs.java

--- minecraft2/src/Mobs.java
+++ minecraft2/src/Mobs.java
@@ -8,6 +8,16 @@ public class Mob {
     private String item;
     private Integer talktimes;
 
+    /*
+     * @param locations
+     * @return An arraylist that contains the rooms that a mob can appear in
+     * @param dialogue
+     * @return An arraylist that contains a mob's dialogue 
+     * @param item
+     * @return The item that a specific mob can be given by the player
+     * @param position
+     * @return The room that the mob is currently in
+     */
     public Mob  (String name, ArrayList<Room> locations, ArrayList<String> dialogue, String item, String position) {
         this.name = name;
         this.locations = locations;
@@ -16,18 +26,22 @@ public class Mob {
         this.item = item;
     }
 
+    //Returns a mob's current position
     public String getPosition(){
         return position;
     }
 
+    //Returns a mob's list of potential locations
     public ArrayList<Room> getLocations(){
         return locations;
     }
 
+    //Returns a mob's name
     public String getName(){
         return name;
     }
 
+    //The player attempts the pet a mob, if the mob is the cat then it will return unique dialogue, otherwise will return another statement
     public String pet() {
         if(this.name.equals("Cat")) {
             return("The cat hacks up a hairball.");
@@ -37,6 +51,9 @@ public class Mob {
         }
     }
 
+    /*The player tries to give a mob an item. If the item is an item the mob can accept, will return the mob's given item dialogue 
+     * Otherwise will return a statement saying that the mob does not want the item
+    */
     public String give(Item item) {
         if(item.getName().equals(this.item)) {
             return this.dialogue.get(0);
@@ -46,6 +63,8 @@ public class Mob {
             return("They do not want this item.");
         }
     }
+    
+    //Returns a dialogue piece from a mob's dialogue list. Will iterate through the list properly
     public String talk() {
         talktimes += 1;
         if(talktimes >= dialogue.size()){
