eaf8809d14ac01a1dc254f7a9809101e7e263f8d
audmaxwell <49968445+audmaxwell@users.noreply.github.com>
2019-10-06 14:21:47 -0400

    Update Items.java

--- minecraft2/src/Items.java
+++ minecraft2/src/Items.java
@@ -1,31 +1,51 @@
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
-public class Item {
+public class Room {
 	private String name;
 	private String desc;
-	private ArrayList<Room> use_room;
-	
-	public Item(String name, String description, ArrayList<Room> use_room) {
+	private HashMap<String, Room> exits;
+	private ArrayList<Item> items;
+
+	public Room(String name, String desc, HashMap<String, Room> exits, ArrayList<Item> items) {
 		this.name = name;
-		desc = description;
-		this.use_room = use_room;
-		
+		this.desc = desc;
+		this.exits = exits;
+		this.items = items;
 	}
-	
-	
-			
-	
-	public String examine() {
-		return desc;
+	public ArrayList<Item> getItems(){
+		return items;
 	}
-	
-	public void use() {
-		if(use_room.contains(Player.Room)) {
-			
-		}else {
-			System.out.println("This item cannot be used in this room.");
+
+	public String getName(){
+		return this.name;
+	}
+
+	public String look() {
+		String output = desc;
+		if(items.size() == 0){
+			output += " There aren't any items you can take from this area.";
 		}
-		
+		else{
+			output += " Items in area: ";
+			for(Item item : items){
+				output += item.getName() + ", ";
+			}
+			output = output.substring(0, output.length() - 2);
+		}
+		return output;
+	}
+
+	public HashMap<String, Room> getExits(){
+		return exits;
+	}
+
+	public ArrayList<String> exit() {
+		ArrayList<String> directions = new ArrayList<>();
+		for(String key : exits.keySet()){
+			directions.add(key + " : " + exits.get(key).getName());
+		}
+		return directions;
 	}
 }
