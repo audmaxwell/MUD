5afc09cba2456c7cd1474e183107312d8fd8634f
audmaxwell <49968445+audmaxwell@users.noreply.github.com>
2019-10-29 14:14:44 -0400

    Update Item.java

--- minecraft2/src/Item.java
+++ minecraft2/src/Item.java
@@ -4,14 +4,7 @@ public class Item {
     private String use_room;
     private String use;
 
-/*
- * @param description
- * @return A general description of an the item
- * @param use_room
- * @return The room a particular item can be used in
- * @param use
- * @return A system dialogue describing what happens when an item is used
- */
+
     public Item (String name, String description, String use_room, String use) {
         this.name = name;
         this.desc = description;
@@ -20,22 +13,19 @@ public class Item {
 
     }
 
-    //Returns an item's name
+
     public String getName(){
         return this.name;
     }
-    
-    //Returns an item's use statement
+
     public String getUse(){
         return this.use;
     }
 
-    //Returns an item's description
     public String examine() {
         return this.desc;
     }
 
-    //Returns an item's use room
     public String getUseRoom() {
         return use_room;
     }
