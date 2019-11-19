b1b498ff6e01f88ba9e3794ead8eb4cff13af2a6
audmaxwell <49968445+audmaxwell@users.noreply.github.com>
2019-11-19 14:29:14 -0500

    Update Mob.java

--- minecraft2/src/Mob.java
+++ minecraft2/src/Mob.java
@@ -1,75 +1,45 @@
 import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Random;
 
-public class Mob {
+public class Mob implements Runnable, Observer {
     private String name;
-    private ArrayList<Room> locations;
-    private String position;
+    private Room room;
     private ArrayList<String> dialogue;
-    private String item;
-    private Integer talktimes;
+    private float moveTimer;
+    private Boolean isTalking;
+    private float starttime;
 
-    /*
-     * @param locations
-     * @return An arraylist that contains the rooms that a mob can appear in
-     * @param dialogue
-     * @return An arraylist that contains a mob's dialogue 
-     * @param item
-     * @return The item that a specific mob can be given by the player
-     * @param position
-     * @return The room that the mob is currently in
-     */
-    public Mob  (String name, ArrayList<Room> locations, ArrayList<String> dialogue, String item, String position) {
+    public Mob(String name, Room room, ArrayList<String> dialogue){
+        this.room = room;
         this.name = name;
-        this.locations = locations;
-        this.position = position;
         this.dialogue = dialogue;
-        this.item = item;
+        starttime = System.currentTimeMillis();
+
     }
 
-    //Returns a mob's current position
-    public String getPosition(){
-        return position;
-    }
+    @Override
+    public void run() {
+        Random num = new Random();
+        float timePassed = 0;
+        moveTimer = (num.nextInt(10) + 10) * 1000;
+        while(true){
+            if (System.currentTimeMillis() > starttime + moveTimer) {
 
-    //Returns a mob's list of potential locations
-    public ArrayList<Room> getLocations(){
-        return locations;
-    }
-
-    //Returns a mob's name
-    public String getName(){
-        return name;
-    }
-
-    //The player attempts the pet a mob, if the mob is the cat then it will return unique dialogue, otherwise will return another statement
-    public String pet() {
-        if(this.name.equals("Cat")) {
-            return("The cat hacks up a hairball.");
-        }
-        else {
-            return("You are asked politely to stop.");
-        }
-    }
-
-    /*The player tries to give a mob an item. If the item is an item the mob can accept, will return the mob's given item dialogue 
-     * Otherwise will return a statement saying that the mob does not want the item
-    */
-    public String give(Item item) {
-        if(item.getName().equals(this.item)) {
-            return this.dialogue.get(0);
+            }
 
         }
-        else {
-            return("They do not want this item.");
-        }
+
     }
-    
-    //Returns a dialogue piece from a mob's dialogue list. Will iterate through the list properly
-    public String talk() {
-        talktimes += 1;
-        if(talktimes >= dialogue.size()){
-            return this.dialogue.get(dialogue.size() - 1);
-        }
-        return this.dialogue.get(talktimes);
+
+    private void moveMob(){
+        Random random = new Random();
+        int roomnd = random.nextInt(room.getExits().size());
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
