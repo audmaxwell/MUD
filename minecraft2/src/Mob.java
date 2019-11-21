0683abb170662534e3bc6a4e3881b595d5e6c5aa
audmaxwell <49968445+audmaxwell@users.noreply.github.com>
2019-11-21 12:17:36 -0500

    Update Mob.java

--- minecraft2/src/Mob.java
+++ minecraft2/src/Mob.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
@@ -7,25 +8,45 @@ public class Mob implements Runnable, Observer {
     private String name;
     private Room room;
     private ArrayList<String> dialogue;
-    private float moveTimer;
-    private Boolean isTalking;
-    private float starttime;
+    private long moveTimer;
+    private Boolean playerinRoom;
+    private long starttime;
+    private Game game;
+    private ImageIcon image;
 
-    public Mob(String name, Room room, ArrayList<String> dialogue){
+    public Mob(String name, Room room, ArrayList<String> dialogue, Game game, ImageIcon image){
         this.room = room;
         this.name = name;
+        this.game = game;
+        this.image = image;
         this.dialogue = dialogue;
         starttime = System.currentTimeMillis();
+        playerinRoom = false;
 
+    }
+    public ImageIcon getImage(){
+        return image;
+    }
+    public String talk(){
+        if(this.name.equals("Cat")){
+            return dialogue.get(0);
+        }
+        else{
+            Random rand = new Random();
+            return dialogue.get(rand.nextInt(dialogue.size()));
+        }
     }
 
     @Override
     public void run() {
         Random num = new Random();
-        float timePassed = 0;
-        moveTimer = (num.nextInt(10) + 10) * 1000;
+        moveTimer = (num.nextInt(11) + 10) * 1000;
         while(true){
             if (System.currentTimeMillis() > starttime + moveTimer) {
+                if(!playerinRoom){
+                    moveMob();
+                    starttime = System.currentTimeMillis();
+                }
 
             }
 
@@ -36,10 +57,35 @@ public class Mob implements Runnable, Observer {
     private void moveMob(){
         Random random = new Random();
         int roomnd = random.nextInt(room.getExits().size());
+        int i = 0;
+        String direction = "";
+        for(String s: room.getExits().keySet()){
+            if(i == roomnd){
+                direction = s;
+            }
+            else{
+                i++;
+            }
+        }
+        Room newroom = room.getExits().get(direction);
+        room.removeMob(this);
+        newroom.addMob(this);
+        room = newroom;
+        game.someoneMoves();
+
+
     }
 
     @Override
     public void update(Observable o, Object arg) {
+        if(game.isOver){
+            System.exit(0);
+        }
+        else{
+            playerinRoom = game.player.getRoomobj() == room;
+
+        }
+
 
     }
 }
